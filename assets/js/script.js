1172666607fd86148fb0e23eff03e86ea58cf5c1
napling <matthewcellis98@gmail.com>
2024-08-07 21:42:03 -0500

    got data to display

---
+++ assets/js/script.js
@@ -0,0 +1,34 @@
+console.log('hello world!');
+const everyOrg = `https://partners.every.org/v0.2/search/AL?apiKey=pk_live_ed857e1af5a6567d7354ed4554625a24`;
+const resultEl = document.getElementById('results');
+
+const searchForOrgs = function (location) {
+    const everyOrgSearch = `https://partners.every.org/v0.2/search/${location}?apiKey=pk_live_ed857e1af5a6567d7354ed4554625a24`;
+
+    fetch(everyOrgSearch)
+        .then(function (response) {
+            if (response.ok) {
+                response.json().then(function (data) {
+                    console.log(data)
+                    for (let i = 0; i < data.nonprofits.length; i++) {
+                        renderData(data.nonprofits[i])
+                    }
+                })
+            };
+        });
+};
+
+const renderData = function (data) {
+    const h1 = document.createElement('h1');
+
+    h1.textContent = data.name
+    resultEl.appendChild(h1);
+}
+
+// create function that stores search results in local data
+
+// create function that pulls search results from local data
+
+searchForOrgs(`Nashville`)
+
+console.log(results);
