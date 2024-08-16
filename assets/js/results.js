f0039560050e432c22f773f71cf9ded41c553e08
napling <matthewcellis98@gmail.com>
2024-08-15 19:02:19 -0500

    unsave button

--- assets/js/results.js
+++ assets/js/results.js
@@ -32,16 +32,16 @@ const loadNonprofits = function () {
         const containerEl = document.createElement('div');
         const infoEl = document.createElement('button');
         const starEl = document.createElement('button');
-        
+
         containerEl.setAttribute('data-ein', nonprofit.ein);
         li.setAttribute('class', 'flex direction-row justify-between');
         containerEl.setAttribute('class', 'flex direction-row');
-        
+
         h3.textContent = nonprofit.name;
         infoEl.textContent = '🔍';
         infoEl.setAttribute('class', 'pr-5');
         starEl.textContent = '⭐';
-        
+
         // Append them to the container
         li.appendChild(h3);
         containerEl.appendChild(infoEl);
@@ -58,12 +58,12 @@ function openModal() {
 
 resultsEl.addEventListener('click', function (event) {
     const clicked = event.target
-    
+
     // When the 'save search' button is clicked, the functions will run to make the list item appear in the saved searches container
     if (clicked.matches('button') && clicked.textContent === '⭐') {
         const ein = clicked.parentElement.getAttribute('data-ein');
         fetch(`https://partners.every.org/v0.2/search/${ein}?apiKey=pk_live_ed857e1af5a6567d7354ed4554625a24`)
-        .then(function (response) {
+            .then(function (response) {
                 if (response.ok) {
                     response.json().then(function (data) {
                         const nonprofit = data.nonprofits[0]
@@ -72,6 +72,81 @@ resultsEl.addEventListener('click', function (event) {
                     })
                 }
             })
+    }
+
+    // When the 'more information' button is clicked, information will be called from the Charity API and will be appended to the modal
+    if (clicked.matches('button') && clicked.textContent === '🔍') {
+        const apiUrl = 'https://api.charityapi.org/api/organizations';
+        const apiKey = 'live-5xe2vl-WAWxO6mV2GxmymmvPTC1glt2LnWv2PfbKBRbFFiW6b9vzUV5GUVRQpXlGCL09YdjwXmwZ-aV9';
+        const ein = clicked.parentElement.getAttribute('data-ein');
+        const searchUrl = `https://api.charityapi.org/api/organizations/${ein}`
+
+        fetch(searchUrl, {
+            method: 'GET',
+            headers: {
+                'apiKey': apiKey
+            }
+        })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Failed to fetch organization data');
+                }
+                return response.json();
+            })
+            .then(result => {
+
+                console.log(result.data);
+
+                // Create the elements and render the data
+                const details = document.getElementById('details');
+
+                details.innerHTML = ''
+
+                const address = document.createElement('p');
+                const city = document.createElement('p');
+                const state = document.createElement('p');
+                const zip = document.createElement('p');
+
+                address.textContent = result.data.street;
+                city.textContent = result.data.city;
+                state.textContent = result.data.state;
+                zip.textContent = result.data.zip;
+
+                details.appendChild(address);
+                details.appendChild(city);
+                details.appendChild(state);
+                details.appendChild(zip);
+
+                // Run the function to open the modal
+                openModal();
+
+            })
+            .catch(error => {
+                console.error('Error fetching organization data:', error);
+            });
+    }
+
+}
+)
+
+savedEl.addEventListener('click', function (event) {
+    const clicked = event.target
+
+    // When the 'save search' button is clicked, the functions will run to make the list item appear in the saved searches container
+    if (clicked.matches('button') && clicked.textContent === '⭐') {
+        const ein = clicked.parentElement.getAttribute('data-ein');
+        const savedNonprofits = JSON.parse(localStorage.getItem('savedNonprofits')) || []
+        // console.log(savedNonprofits);
+        for (let i = 0; i < savedNonprofits.length; i++) {
+            if (ein === savedNonprofits[i].ein) {
+                delete savedNonprofits[i];
+                i++;
+            } else {
+                i++
+            }
+        }
+        // savedNonprofits.filter(Boolean)
+        localStorage.setItem('savedNonprofits', JSON.stringify(savedNonprofits))
     }
 
     // When the 'more information' button is clicked, information will be called from the Charity API and will be appended to the modal
